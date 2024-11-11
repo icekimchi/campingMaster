61888ea4e37970320d5c1f96c0d0606fb1dad8a9
icekimchi <pjh010828@gmail.com>
2024-11-12 01:12:43 +0900

    fix: rebase conflict 해결
    브랜치 꼬인거 해결..
    옛날 코드랑 비교하면서 confict 부분 모두 해결하였다.
    죽을 것 같다 :)

--- app/src/main/java/com/example/campingmaster/fragment/ChatFragment.java
+++ app/src/main/java/com/example/campingmaster/fragment/ChatFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.campingmaster.BuildConfig;
 import com.example.campingmaster.R;
 import com.example.campingmaster.adapter.MessageAdapter;
 import com.example.campingmaster.model.Message;
@@ -44,6 +45,9 @@ public class ChatFragment extends Fragment {
 
     private List<Message> messageList;
     private MessageAdapter messageAdapter;
+    private static final String API_KEY = BuildConfig.GPT_API;
+    private static final String API_ENDPOINT = BuildConfig.GPT_ENDPOINT;
+
 
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
